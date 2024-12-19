1980b8de5691ff613d6ffeb4630b3fd0c42a4a8e
cgruver <cgruver@clg.lab>
2024-12-19 17:34:50 +0000

    remove target

--- target/repo/modules/ansible/install.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-pip3 install ansible-navigator
-pip3 install ansible
-pip3 install ansible-lint
-
-chmod -R g=u ${HOME}
--- target/repo/modules/podman-compose/install.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-pip3 install podman-compose
-
-chmod -R g=u ${HOME}
